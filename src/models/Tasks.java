c2c95b9fdfeb8c7cc132b21dcc7a43fcbfadfcf3
masako.takehara <mt82843@gmail.com>
2024-04-30 12:33:17 +0900

    MySQLとHibernateのセットアップ

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 @Entity
 
 @Table(name = "tasks")
-public class Tasklist {
+public class Tasks {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
